f5453abd9f1c30fff43d7e67b8c78da5acbdb12b
Wendy Le <linhkhanh1127@gmail.com>
2021-02-16 20:29:03 -0500

    addingfiles

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const path = require('path');
 
 const messenger = require('socket.io')();
@@ -25,6 +26,14 @@ messenger.attach(server);
 //socket is the individual connection - the caller
 messenger.on('connection', (socket) => {
 	console.log(`a user connected: ${socket.id}`);
+
+	//send the connected user their assigned ID
+	socket.emit('connected', { sID:`${socket.id}`, message: 'new connection'});
+
+	socket.on('chatmessage', function(msg){
+		console.log(msg);
+		messenger.emit('message', { id: socket.id, message: msg });
+	});
 
 	socket.on('disconnect', () => {
 		console.log('a user has disconnected');
